41818a668e82c111f75e11002b3cf41d3ac8bf57
Michael Stock-Matthews <mstockmatthews@gmail.com>
2012-04-07 00:30:47 -0700

    Adds registration and email confirmation endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flaskext.mail import Mail
+from flaskext.mail import Mail, Message
 from flask import Flask, request, session, render_template, redirect, url_for, flash
 from models import User
 
@@ -35,14 +35,66 @@ def authenticate(e, p):
 
 @app.route('/register', methods=['POST'])
 def register():
-    school = request.form.get('school')
+    error = None
     email = request.form.get('email')
     password = request.form.get('password')
+
+    school = emailAuth(email)
+    if not school:
+        error = 'Invalid email address'
 
     user = User(school,email,password)
     db.session.add(user)
     db.session.commit()
 
+    sendConfirmation(user.id,email)
+
     session['user_id'] = user.id
 
-    return redirect(url_for('app.home'))
+    return redirect(url_for('index'),error=error)
+
+def sendConfirmation(id,email):
+    confkey = generateUnique()
+
+    link = '<p><a href=%s/confirm?confkey=%s' % (base_url,confkey)
+    body = '<p>Please confirm your email address by clicking the link below:</p>' + link)
+    subj = '5C Word Warp - Email Confirmation'
+
+    r.set(confkey,id)
+
+    msg = Message(html=body,subject=subj,recipients=[email])
+    mail.send(msg)
+
+@app.route('/confirm', methods=['GET'])
+def confirm():
+    error = None
+    key = request.args.get('confkey')
+
+    if key = None:
+        error = 'Invalid confirmation key' 
+    id = r.get(key)
+    if id = None:
+        error = 'No such user'
+
+    user = User.query.get(id)
+    user.verified = True
+    flash('Your email address is confirmed! Thanks!')
+    return redirect(url_for('index'),error=error)
+
+
+def emailAuth(e):
+    e = e.lower()
+    school = None
+
+    if e.endswith('pomona.edu'):
+        school = 'po'
+    elif e.endswith('hmc.edu'):
+        school = 'hm'
+    elif e.endwith('pitzer.edu'):
+        school = 'pz'
+    elif e.endswith('scrippscollege.edu'):
+        school = 'sc'
+    elif e.endswith('cmc.edu') or e.endswith ('claremontmckenna.edu'):
+        school = 'cm'
+
+    return school
