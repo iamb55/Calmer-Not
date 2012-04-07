b92f3f56a9aa2735390f8c5608997223e3f3fe13
Mark Hudnall <landakram@gmail.com>
2012-04-07 04:06:02 -0700

    add secret key

--- settings.py
+++ settings.py
@@ -7,3 +7,5 @@ MAIL_USE_SSL = True
 MAIL_USERNAME = '5cwordwarp@gmail.com'
 MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
 DEFAULT_MAIL_SENDER = '5cwordwarp@gmail.com'
+
+SECRET_KEY = os.environ['SECRET_KEY']
