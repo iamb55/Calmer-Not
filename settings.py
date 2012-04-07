8e6cdfdfca8ba5e2ef05fdc0d8dd8f30b723814e
Mark Hudnall <landakram@gmail.com>
2012-04-07 03:58:56 -0700

    mail pass

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
+import os
 DEBUG = True
 
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_SSL = True
 MAIL_USERNAME = '5cwordwarp@gmail.com'
+MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
 DEFAULT_MAIL_SENDER = '5cwordwarp@gmail.com'
